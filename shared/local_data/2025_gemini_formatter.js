ac047f20042cf05627f47170c49a7b23c78e960d
Cory Sougstad <cory@hau.me>
2025-01-17 12:27:11 -0700

    Nuke the api key

--- shared/local_data/2025_gemini_formatter.js
+++ shared/local_data/2025_gemini_formatter.js
@@ -1,10 +1,9 @@
 const {
   GoogleGenerativeAI,
-  HarmCategory,
   HarmBlockThreshold,
 } = require("@google/generative-ai");
 
-const apiKey = "AIzaSyD-jHvfafMSswoSS-0Ut750b_MNZjf97qw";
+const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
